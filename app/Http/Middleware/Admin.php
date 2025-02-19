4ceef41125c45d5e9463e5ee39c60d1d538f4944
AnnurIbrahim <annur.ibrahim.jagoub.adam@student.gildeopleidingen.nl>
2025-02-19 11:30:49 +0100

    Update Admin.php

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -1,26 +1,20 @@
 <?php
 
-// app/Http/Middleware/Admin.php
-
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-// use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
 
-class Admin
+class admin
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->role === 'admin') {
-            return $next($request);
-        }
-        return redirect('/login')->withErrors('Je hebt geen toegang tot deze pagina.');
-}
+        return $next($request);
+    }
 }
